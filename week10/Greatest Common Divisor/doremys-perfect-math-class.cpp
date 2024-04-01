40b35d274e1db07c503ea145a0b300166d4bebc5
Steven Y M Chang <stevenymchang@wirelessprv-10-192-170-139.near.illinois.edu>
2024-04-01 14:44:06 -0500

    starting doremys perfect math class

---
+++ week10/Greatest Common Divisor/doremys-perfect-math-class.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+/* Doremy's Perfect Math Class
+"Everybody! Doremy's Perfect Math Class is about to start! Come and do your best if you want to have as much IQ as me!" 
+In today's math class, Doremy is teaching everyone subtraction. Now she gives you a quiz to prove that you are paying attention in class.
+
+You are given a set 𝑆 containing positive integers. You may perform the following operation some (possibly zero) number of times:
+    choose two integers 𝑥 and 𝑦 from the set 𝑆 such that 𝑥>𝑦 and 𝑥−𝑦 is not in the set 𝑆.
+    add 𝑥−𝑦 into the set 𝑆.
+You need to tell Doremy the maximum possible number of integers in 𝑆 if the operations are performed optimally. 
+It can be proven that this number is finite.
+
+Input: The input consists of multiple test cases. The first line contains a single integer 𝑡 (1≤𝑡≤10^4) — the number of test cases. 
+The description of the test cases follows.
+
+The first line contains an integer 𝑛 (2≤𝑛≤10^5) — the size of the set 𝑆.
+
+The second line contains 𝑛 integers 𝑎1,𝑎2,…,𝑎𝑛 (1≤𝑎1<𝑎2<⋯<𝑎𝑛≤10^9) — the positive integers in 𝑆.
+
+It is guaranteed that the sum of 𝑛 over all test cases does not exceed 2⋅10^5.
+
+Output: For each test case, you need to output the maximum possible number of integers in 𝑆. It can be proven that this value is finite.
+*/
+
+int main(){
+
+}
