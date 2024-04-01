abfc319f3665945e27f9090b1c034b41a0e61987
Steven Y M Chang <stevenymchang@Stevens-MacBook-M3Pro.local>
2024-04-01 18:24:32 -0500

    completed euclideans and bezouts

--- week10/Greatest Common Divisor/extended-euclidean-algorithm-and-bezouts-identity.cpp
+++ week10/Greatest Common Divisor/extended-euclidean-algorithm-and-bezouts-identity.cpp
@@ -18,9 +18,36 @@ Input: The first and only line contains integers 𝑎 and 𝑏 and 𝑐 with 1�
 Output: Output a pair of space seperated integers 𝑥 and 𝑦 where 𝑥 is the minimum 𝑥≥0 that satisfies 𝑎𝑥+𝑏𝑦=𝑐.
 */
 
+// helper function to find the greatest common divisor between two numbers
+int greatestCommonDivisor(ll valueOne, ll valueTwo){
+    if(valueTwo > valueOne) return greatestCommonDivisor(valueTwo, valueOne);
+    if(valueTwo > 0) return greatestCommonDivisor(valueTwo, valueOne % valueTwo);
+    else return valueOne;
+}
+
 int main(){
     ll a, b, c;
     cin >> a >> b >> c;
-
     
+    // find gcd of a and b and initialize x to 0
+    ll gcdAB = greatestCommonDivisor(a, b);
+    ll x = 0;
+    ll y;
+
+    // if c is not a multiple of GCD(a, b), then it is not possible (based on Bezout's Identity)
+    if(c % gcdAB != 0){
+        cout << -1 << endl;
+    } else{
+    // once we can confirm that ax + by = c holds true for some x and y values, we loop through possible x values from 0 (inclusive) and check if the resulting y is an integer
+        while(1){
+            // if ax + by = c holds for current x value, then find the corresponding y value and break
+            if((c - a*x) % b == 0){
+                y = (c - x*a) / b;
+                break;
+            }
+            // otherwise, try the next x value
+            x++;
+        }
+        cout << x << " " << y << endl;
+    }
 }
