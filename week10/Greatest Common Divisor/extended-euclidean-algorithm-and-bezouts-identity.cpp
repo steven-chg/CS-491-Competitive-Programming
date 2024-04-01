5c332a54c3bb8a08f442e57cc0429c183886aea7
Steven Y M Chang <stevenymchang@Stevens-MacBook-M3Pro.local>
2024-04-01 17:28:36 -0500

    starting extended euclidean problem

---
+++ week10/Greatest Common Divisor/extended-euclidean-algorithm-and-bezouts-identity.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+/* Extended Euclidean Algorithm and Bezout's Identity
+Bézout's identity states that for any positive integer 𝑎 and 𝑏, there exists integers 𝑥 and 𝑦 such that:
+[ax+by = GCD(a,b).]
+
+What if we are given integers 𝑎, 𝑏, and 𝑐, and we wish to find integers 𝑥 and 𝑦 such that:
+[ax+by = c.]
+
+Find the pair (𝑥,𝑦) satisfying this equation with the minimum possible 𝑥≥0 or determine that this is not possible.
+
+Input: The first and only line contains integers 𝑎 and 𝑏 and 𝑐 with 1≤𝑎,𝑏,𝑐≤10^9.
+
+Output: Output a pair of space seperated integers 𝑥 and 𝑦 where 𝑥 is the minimum 𝑥≥0 that satisfies 𝑎𝑥+𝑏𝑦=𝑐.
+*/
+
+int main(){
+    ll a, b, c;
+    cin >> a >> b >> c;
+
+    
+}
